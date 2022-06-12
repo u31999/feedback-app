32e81d60be11d4fa7e7975cb8b575b42e75628dc
u31999 <u31999@gmail.com>
2022-06-12 14:07:57 -0700

    Add styled component, and delete function

--- src/App.js
+++ src/App.js
@@ -1,6 +1,25 @@
+import { useState } from "react";
+import Header from "./component/Header";
+import FeedbackList from "./component/FeedbackList";
+import FeebackData from "./data/FeedbackData";
+
 function App () {
+    const [feedback, setFeedback] = useState(FeebackData)
+    const deleteFeedback = (id) => {
+        if(window.confirm('Are you sure you want to DELETE this!'))
+        {
+            setFeedback(feedback.filter((item) => item.id !== id))
+        }
+    }
+
     return(
-        <h1>My App Component</h1>
+        <>
+        <Header />
+        <div className="container">
+        <FeedbackList feedback={feedback}
+        handleDelete={deleteFeedback} />
+        </div>
+        </>
     )
 }
 
