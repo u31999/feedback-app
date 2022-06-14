b38583ea269f5c2aa40cf95773e0164681876b19
u31999 <u31999@gmail.com>
2022-06-14 10:01:37 -0700

    Add animation to FeedbackList, using framer-motion module

--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -15,7 +15,7 @@ function Header({ text, bgColor, textColor }) {
 }
 
 Header.defaultProps = {
-  text: 'Feeback UI',
+  text: 'Feedback UI',
   bgColor: 'rgba(0, 0, 0, 0.4',
   textColor: '#ff6a95'
 }
